2ad48c6424cbe0edd82f0c27a32dcdcea1430327
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-07 10:01:10 -0600

    Initial Code added

---
+++ getFristItemFrom.js
@@ -0,0 +1,9 @@
+// # You Deserve Arrays
+
+// ### Introduction
+
+// This is an exercise for retrieving data from an array. Arrays are great and you will soon love them!
+
+// ### Challenges
+
+// 1. Write a program, `getFirstItemFrom.js` that prompts the user for an array, then logs out the first item in that
