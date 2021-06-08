4b2ec624104abf41ac84c24cc507fb0d15df4232
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-08 08:45:17 -0600

    Finished Code with .split function|

---
+++ secondCharofThirdString.js
@@ -0,0 +1,20 @@
+// 6. `secondCharOfThirdString.js`: prompts the user for an array then logs the second character in the third item in the array. If the third item is not a string, log an error instead
+
+
+//Call prompt-sync
+const prompt = require('prompt-sync')();
+
+//Define terms
+let userArray = prompt('Please enter three items that you need this week for groceries.');
+
+let splitString = userArray.split(' ');
+
+let newSplit = splitString[2];
+
+
+
+
+console.log(splitString);
+
+console.log(newSplit[1]);
+// console.log(splitString[1]);
